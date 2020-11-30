408fc9980a28cc12054b140d8795ed536f944722
Jake Haberle <59548398+jhaberle@users.noreply.github.com>
2020-11-30 00:39:47 -0700

    added hashrouter to try to get deploy working

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ const About = () => {
         Josh (4),and Dylan (2). Being a dad is awesome, challenging and by far
         the coolest thing I've ever done. If I ever have free time I love
         hitting the slopes, playing some golf, and watching all of the Denver
-        spots teams.
+        sports teams.
         <br />
         <br />I currently work at Empower Retirement in Investment Operations as
         a Valuations Analyst. I'm thrilled to have the opportunity to learn a
